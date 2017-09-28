0d055a713eb9601736d191fbdf56cb73b9054424
ab841673 <ab841673@MI121-01>
2017-09-28 17:58:19 +0200

    Change main for vueConsole

--- Panier/Panier/src/main/java/fr/ufrsciencestech/panier/App.java
+++ Panier/Panier/src/main/java/fr/ufrsciencestech/panier/App.java
@@ -8,15 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        Orange o1 = new Orange(5,"France");
-        Orange o2 = new Orange(6,"Espagne");
-        Orange o3 = new Orange(7,"Italie");
-        
         Panier p1 = new Panier(20);
         
+        vueConsole vue = new vueConsole();
+        
+        p1.addObserver(vue);
+        
         p1.ajoute();
-        
-        
-        
+        p1.ajoute();
     }
 }
